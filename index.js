d9b58cc7dff94128f5c222ce4594eb5a87a78eff
Hazar Saad ElDine <saadeldinehazar@gmail.com>
2021-04-14 11:32:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
 let dodger = document.getElementById("dodger");
 
+function moveDodgerRight() {
+  let right.Numbers = dodger.style.right.replace("px", "");
+  let right = parseInt(leftNumbers, 10);
+
+  if (right > 0) {
+    dodger.style.right = `${right +1}px`;
+  }
+}
+
+document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
 function moveDodgerLeft() {
-  let leftNumbers = dodger.style.left.replace("px", "10");
+  let leftNumbers = dodger.style.left.replace("px", "");
   let left = parseInt(leftNumbers, 10);
 
   if (left > 0) {
-    dodger.style.left = `${left - 1}px`;
+    dodger.style.left = `${left -1}px`;
   }
 }
 
 document.addEventListener("keydown", function(e) {
   if (e.key === "ArrowLeft") {
-    moveDodgerLeft();
+    moveDodgerRight();
   }
 });
